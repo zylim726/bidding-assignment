5b188d0f882276209cd4f95d54fd779bc9b9033c
zylim726 <zylim@metrio.com.my>
2026-08-28 10:58:55 +0800

    fix laravel compiled view path

--- backend/config/view.php
+++ backend/config/view.php
@@ -2,33 +2,10 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views.
-    |
-    */
-
     'paths' => [
         resource_path('views'),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where the compiled Blade templates will be
-    | stored.
-    |
-    */
-
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'compiled' => storage_path('framework/views'),
 
 ];
